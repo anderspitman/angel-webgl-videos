4fb3311f7ffe90f15eb58955d1aacc25bebf1d23
Anders Pitman <tapitman11@gmail.com>
2017-03-18 11:58:02 -0700

    Started implementing play/pause and progress text

--- src/components/VideoControl.jsx
+++ src/components/VideoControl.jsx
@@ -6,11 +6,23 @@ export class VideoControl extends React.Component {
     return (
       <div>
         <div>Playback Speed: {this.props.playbackRate}</div>
+        <input type="button" value="Play/Pause"
+            onClick={this.props.onPlayPause}></input>
+        <ProgressText currentTime={this.props.currentTime} 
+            totalTime={this.props.totalTime} />
         <input type="button" value="Increase Speed"
             onClick={this.props.onIncreasePlaybackRate}></input>
         <input type="button" value="Decrease Speed"
             onClick={this.props.onDecreasePlaybackRate}></input>
       </div>
+    );
+  }
+}
+
+class ProgressText extends React.Component {
+  render() {
+    return (
+      <span>{this.props.currentTime} / {this.props.totalTime}</span>
     );
   }
 }
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -8,8 +8,13 @@ export class VideoPlayer extends React.Component {
 
     this.playbackRateStep = 0.25;
 
-    this.state = { playbackRate: 1.0 };
+    this.state = {
+      playbackRate: 1.0,
+      currentTime: 0.0,
+      totalTime: 0.0
+    };
 
+    this.onPlayPause = this.onPlayPause.bind(this);
     this.onIncreasePlaybackRate = this.onIncreasePlaybackRate.bind(this);
     this.onDecreasePlaybackRate = this.onDecreasePlaybackRate.bind(this);
   }
@@ -23,11 +28,22 @@ export class VideoPlayer extends React.Component {
         <video style={style} ref='video' controls preload="auto" >
           <source src={this.props.link} type="video/mp4"></source>
         </video>
-        <VideoControl playbackRate={this.state.playbackRate}
+        <VideoControl
+            currentTime={this.state.currentTime}
+            totalTime={this.state.totalTime}
+            playbackRate={this.state.playbackRate}
+            onPlayPause={this.onPlayPause}
             onIncreasePlaybackRate={this.onIncreasePlaybackRate}
             onDecreasePlaybackRate={this.onDecreasePlaybackRate} />
       </div>
     );
+  }
+
+  componentDidMount() {
+    //this.refs.video.controls = false;
+    this.setState({
+      currentTime: this.refs.video.currentTime
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -36,6 +52,15 @@ export class VideoPlayer extends React.Component {
       this.refs.video.play();
     }
     this.refs.video.playbackRate = this.state.playbackRate;
+  }
+
+  onPlayPause() {
+     if (this.refs.video.paused || this.refs.video.ended) {
+       this.refs.video.play();
+     }
+     else {
+       this.refs.video.pause();
+     }
   }
 
   onIncreasePlaybackRate() {
